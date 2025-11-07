459dff7309fe0348a0fdf06389545394f6de94e1
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-11-06 20:30:48 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("hello")
+users_age=int(input("Enter your age"))
+if users_age>20:
+    print("Then you are 20+ years old")
+elif users_age<10:
+    print(input("Then you are between 0 and 10 years old"))
+else:
+    print("Then you are between 10 and 20 years old")
